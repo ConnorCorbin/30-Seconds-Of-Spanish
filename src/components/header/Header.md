c16e4dd301197c5e3b3879cfea40c44061217061
CorbinCon <corbincon@outlook.com>
2020-02-18 17:09:04 +0000

    feat: Added Header component

---
+++ src/components/header/Header.md
@@ -0,0 +1,17 @@
+Header features:
+
+* Renders Image and title
+
+Consumed components:
+
+* Image
+
+## Data Model
+
+| Configurable Fields | Type | Component | Default | Description |
+| ------------------- | ---- | --------- | ------- | ----------- |
+| title1 | `string` | Header | | Text for title 1 |
+| title2 | `string` | Header | | Text for title 2 |
+| textColor | `string` | Header | | Text color for both title 1 and title 2 |
+| imageUrl | `string` | Image | | Image alt tag |
+| imageAltTag | `string` | Image | | Image alt tag |
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+/* istanbul ignore file */
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createGlobalStyle } from 'styled-components';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from 'serviceWorker';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import App from 'components/app/App.js';
+import Header from 'components/header/header.js';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`;
+
+ReactDOM.render(
+  <Fragment>
+    <GlobalStyle />
+    <Header textColor="" imageUrl='https://via.placeholder.com/150' imageAltTag="This is an alt tag" title1="30 Seconds" title2="of Spanish" />
+  </Fragment>
+, document.getElementById('root'));
